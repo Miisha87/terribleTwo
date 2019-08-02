2a2a9ea610c2a35de0ba24b97046e4db59adb3d0
Your NameLinaleeo <linaer@hotmail.co.uk>
2019-08-02 13:10:58 +0200

    updated about and advice

--- terribleTwos/wwwroot/js/site.js
+++ terribleTwos/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+//<script>
+//// When the user clicks on <div>, open the popup
+
+
